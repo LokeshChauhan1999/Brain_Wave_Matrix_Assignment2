e267a76f34eb15f41e047a571cb91f013733431f
LOKESH CHAUHAN <161297154+LokeshChauhan1999@users.noreply.github.com>
2024-10-10 22:54:15 +0530

    Student Management System
    Student Management System

--- Single_Row_Functions.sql
+++ Single_Row_Functions.sql
@@ -2,3 +2,4 @@ select upper(first_name) from STUDENTS;
 select lower(last_name) from STUDENTS;
 select decode(gender, 'Male', 'M', 'Female', 'F') from STUDENTS;
 select substr(email,1, 30) from STUDENTS; 
+select max(length(first_name)) frim students;

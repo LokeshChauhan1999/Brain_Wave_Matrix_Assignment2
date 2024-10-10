2d76b7db530fd8d0acc45f94787a457100179b39
LOKESH CHAUHAN <161297154+LokeshChauhan1999@users.noreply.github.com>
2024-10-10 22:56:42 +0530

    Student Management System
    Student Management System

--- joins.sql
+++ joins.sql
@@ -5,7 +5,6 @@ join ENROLLMENT e on s.Student_Id = s.Student_Id
 join CLASSES c on c.class_id = e.class_id
 order by student_id;
 
-
 select s.STUDENT_ID, s.FIRST_NAME, s.LAST_NAME, f.amount_paid, f.payment_date
 from students s
 join fees f on s.student_id = f.student_id;
